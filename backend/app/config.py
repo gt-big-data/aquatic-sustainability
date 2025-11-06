3eca315441ac5afc538ee89766ddace3e8657c7d
Rick Lee <135770367+TheNopenator@users.noreply.github.com>
2025-11-06 16:53:35 -0500

    Revert test

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,4 +5,4 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev")
-    GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY", "AIzaSyB371twDkX-IhLmm3cmlpG4nEYxbWwqp-8")
+    GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY", "")
