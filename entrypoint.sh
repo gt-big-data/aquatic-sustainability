c7293475a1457bc691c359ee8af361a4295f360f
Rick <ricklee2487@gmail.com>
2025-12-06 21:50:04 -0500

    replace incorrect name for Earthdata login

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # entrypoint.sh
 echo "machine urs.earthdata.nasa.gov" > ~/.netrc
-echo "login $EARTHDATA_USER" >> ~/.netrc
-echo "password $EARTHDATA_PASS" >> ~/.netrc
+echo "login $EARTHDATA_USERNAME" >> ~/.netrc
+echo "password $EARTHDATA_PASSWORD" >> ~/.netrc
 chmod 600 ~/.netrc
 exec gunicorn --bind :$PORT --workers 2 --threads 8 --timeout 0 run:app
